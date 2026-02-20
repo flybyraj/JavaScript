29b1f7d683a9ee91289a5bb2f4d4705148a99f89
flybyraj <669raj@gmail.com>
2026-02-20 23:24:06 +0530

    Add

--- Lec6/domProperties.js
+++ Lec6/domProperties.js
@@ -4,4 +4,8 @@ console.log(firstEl);
 console.log(firstEl.tagName); //BUTTON ,returns tag for element nodes
 
 //innerText
-console.log(document.body.firstChild);
+console.dir(document.body.firstChild); //Returns text node because a text node is inserted to maintain the whitespace between the  
+                                       //end of the opening of parent and child
+console.log(document.body.firstElementChild); //Returns Element
+console.log(document.querySelector("div"));
+console.log(document.querySelector("div").children); //Acessing Children Nodes
