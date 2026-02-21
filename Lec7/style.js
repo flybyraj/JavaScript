eb4becb25708b978f1e1dee01ef2ac3446e6b4be
flybyraj <669raj@gmail.com>
2026-02-21 19:38:00 +0530

    maintain steak

---
+++ Lec7/style.js
@@ -0,0 +1,12 @@
+//node.style to access the style of Element
+
+let div=document.querySelector("div");
+console.log(div.style); 
+
+div.style.backgroundColor="green";  
+
+div.style.fontSize="26px";
+
+div.innerText="Hello";
+div.style.visibility="hidden";
+
