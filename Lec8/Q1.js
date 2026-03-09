9912ee12731903cea99ee2f67b0dfc98be6a115c
flybyraj <669raj@gmail.com>
2026-03-10 00:24:36 +0530

    add

--- Lec8/Q1.js
+++ Lec8/Q1.js
@@ -1,3 +1,21 @@
 //Qs. Create a toggle button that changes the screen to dark-mode when clicked & light-mode
 //when clicked again.
 
+let mode=window.document.querySelector("button");
+let body = window.document.querySelector("body");
+let currMode="light" // dark
+
+mode.addEventListener("click" , () => {
+    console.log("you are trying to change Mode");
+    if (currMode === "light") {
+        currMode = "dark";
+        body.classlist.add("body");
+        // document.querySelector("body").style.backgroundColor="black";
+        // document.querySelector("body").style.color="white"
+    } else {
+        currMode="light";
+        body.classList.add("light");
+        // document.querySelector("body").style.backgroundColor="white";
+        // document.querySelector("body").style.color="black";
+    }
+})
