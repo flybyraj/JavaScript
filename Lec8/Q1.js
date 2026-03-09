16337ea109d37f29cdf8fa3639f77f71baf42aa5
flybyraj <669raj@gmail.com>
2026-03-09 23:15:02 +0530

    solving q1

--- Lec8/Q1.js
+++ Lec8/Q1.js
@@ -1,11 +1,3 @@
 //Qs. Create a toggle button that changes the screen to dark-mode when clicked & light-mode
 //when clicked again.
 
-let btn1= document.querySelector("button");
-let int =window.document.querySelector("tab");
-let int =document.forthe text.for the quenstance.forthe
-for(int i=0;i<=1;i++) {
-    for the nect of()=>{
-        console.log("hellow.world");
-    }
-}
