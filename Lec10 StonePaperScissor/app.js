e6e4fa19014c9ba067eff1fe811ea89d14070014
flybyraj <669raj@gmail.com>
2026-03-13 22:04:41 +0530

    add

--- Lec10 StonePaperScissor/app.js
+++ Lec10 StonePaperScissor/app.js
@@ -3,15 +3,33 @@ let compScore=0;
 
 const choices= document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+    let options = [ "rock", "paper", "scissors"];
+    const random = options[Math.floor(Math.random() * 3)];
+    return random;
+}
+
+
+
 const playGame = (userChoice) => {
     console.log(`choice was clicked. ${userChoice}`);
     //Generate Computer Choice -> Modular Programming -> har kaam ke liye ek fn.
+    const compChoice = genCompChoice();
+    // if ( userChoice =="rock" && compChoice == "paper") {
+    //     console.log("")
+    // }
+    console.log(`The Computer Choice is ${compChoice}`);
+
+    if (userChoice == compChoice) {
+        console.log("Draw");
+    } else if {
+        
+    }
 }
 
 
 
 choices.forEach((choice) => {
-    // console.log(choice);
     choice.addEventListener("click", () => {
         const userChoice= choice.getAttribute("id");
         playGame(userChoice);
